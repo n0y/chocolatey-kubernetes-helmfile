364e88c7a01bfe26a275e1aca99a4337fe261af9
Bertram Feÿ <bertram.fey@deutschebahn.com>
2025-03-21 14:27:26 +0100

    chore: prepare for Renovate upgrades

--- src/tools/chocolateyinstall.template.ps1
+++ src/tools/chocolateyinstall.template.ps1
@@ -2,11 +2,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName    = 'kubernetes-helmfile'
-    Url            = 'https://github.com/helmfile/helmfile/releases/download/v0.158.1/helmfile_0.158.1_windows_386.tar.gz'
-    Checksum       = 'EAE3F699B3FD2DF4787D15596BEA7C4CD10C47D0057AE3C191C479724A815ED3'
+    Url            = 'https://github.com/helmfile/helmfile/releases/download/v%%VERSION%%/helmfile_%%VERSION%%_windows_386.tar.gz'
+    Checksum       = '%%CHECKSUM%%'
     ChecksumType   = 'sha256'
-    Url64bit       = 'https://github.com/helmfile/helmfile/releases/download/v0.158.1/helmfile_0.158.1_windows_amd64.tar.gz'
-    Checksum64     = '389594024FE8CAD9C70E58B138ECD28C6392CBCFEF7F44ED4AD6194EF1442B7E'
+    Url64bit       = 'https://github.com/helmfile/helmfile/releases/download/v%%VERSION%%/helmfile_%%VERSION%%_windows_amd64.tar.gz'
+    Checksum64     = '%%CHECKSUM64%%'
     ChecksumType64 = 'sha256'
     UnzipLocation = "$toolsDir"
 }
